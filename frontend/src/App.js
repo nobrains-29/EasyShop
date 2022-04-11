96a5fd9bd250284df8e86579aa14974a404a11df
Archit Tripathi <archittripathi94@gmail.com>
2022-04-12 00:24:45 +0530

    Router Added SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to EasyShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
